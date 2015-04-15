32c876fa08654729e43fb021859554db0bea6c1d
Hector Fabio Cadavid Rengifo <hcadavid@Hectors-MacBook-Pro.local>
2015-04-15 13:52:57 -0500

    first commit

--- src/main/webapp/js/libs/appmodule.js
+++ src/main/webapp/js/libs/appmodule.js
@@ -1,7 +1,25 @@
-(function(){
-    var app=angular.module('modone',[]);
-    
-    
+(function () {
+    var app = angular.module('modone', ['ngRoute']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+
+                // route for the about page
+                .when('/new', {
+                    templateUrl: 'registro.html'
+
+                })
+
+                // route for the contact page
+                .when('/list', {
+                    templateUrl: 'catalogo.html'
+
+                });
+
+    });
+
+
+
 })();
 
 
