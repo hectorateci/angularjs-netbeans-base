e23fa3fd2d2a2a40f87c85ee24ee4a70690b3605
Hector <hector.cadavid@gmail.com>
2015-05-21 07:39:04 -0500

    fc

--- src/main/webapp/js/libs/appmodule.js
+++ src/main/webapp/js/libs/appmodule.js
@@ -1,23 +1,6 @@
 (function () {
     var app = angular.module('modone', ['ngRoute']);
 
-    app.config(function ($routeProvider) {
-        $routeProvider
-
-                // route for the about page
-                .when('/new', {
-                    templateUrl: 'registro.html'
-
-                })
-
-                // route for the contact page
-                .when('/list', {
-                    templateUrl: 'catalogo.html'
-
-                });
-
-    });
-
 
 
 })();
